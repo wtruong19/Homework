f6639032803d70f96c9c2daad934b26adf3e5d62
William Truong <william.c.truong@gmail.com>
2015-05-20 11:09:03 -0700

    initial
    problem1

---
+++ seashells.java
@@ -0,0 +1,16 @@
+public class seashells {
+	public static void main(String[] args) {
+		String sen = "she sells seashells by the seashore"; // initial sentence
+		String delim = "[ ]+"; // for find spaces
+		String[] words = sen.split(delim); // split sentence into array of separate words
+		
+		for(int i=0; i<words.length; i++)
+		{
+			if(i%2==1)
+			{
+				words[i] = Character.toUpperCase(words[i].charAt(0))+words[i].substring(1); // capitalize every odd word
+			}
+			System.out.println(words[i]);
+		}
+	}
+}
