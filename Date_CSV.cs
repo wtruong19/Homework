82d81c3dd04acb7e357ef5771a58ac553a041c5a
William Truong <william.c.truong@gmail.com>
2015-05-20 14:52:05 -0700

    initial program3 part B - C#

---
+++ Date_CSV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_CSV
+{
+    class Date_CSV
+    {
+        static void Main(string[] args)
+        {
+            //Create datetime for start and end dates
+            DateTime startCal = new DateTime(2000, 1, 1);
+            DateTime endCal = new DateTime(2004, 12, 31);
+            int quarter;
+
+            // Print out dates in CSV form for each day between the start and end dates
+            for (DateTime start = startCal; start <= endCal; start = start.AddDays(1))
+            {
+                quarter = ((start.Month + 2) / 3); //compute quarter for current date
+                System.Console.WriteLine("{0:yyyy-MM-dd, MMMM}, {1}, {0:dddd}, Q{2}", start, start.DayOfYear, quarter);
+            }
+        }
+    }
+}
